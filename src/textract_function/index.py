c905e7b7468ddaa0c7c0dfc0749df856b44420cd
Bob Strahan <strahans@amazon.com>
2024-11-24 22:15:41 +0000

    (1) add adaptive retries to handle throttling, (2) finer grain timing logs, (3) improve input/output key naming

--- src/textract_function/index.py
+++ src/textract_function/index.py
@@ -5,15 +5,24 @@ import fitz  # PyMuPDF
 from textractor.parsers import response_parser
 import logging
 import time
+from botocore.config import Config
 
-region = "us-east-1"
+
+region = os.environ['AWS_REGION']
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-# Initialize Textract client
-textract_client = boto3.client('textract', region_name=region)
-s3_client = boto3.client('s3')
+# Initialize S3 client
+s3_client = boto3.client('s3', region_name=region)
+# Initialize Textract client with adaptive retry to handle throttling
+config = Config(
+    retries = {
+        'max_attempts': 20,
+        'mode': 'adaptive'
+    }
+)
+textract_client = boto3.client('textract', region_name=region, config=config)
 
 def get_document_text(pdf_content):
     # Open PDF from bytes
@@ -22,13 +31,17 @@ def get_document_text(pdf_content):
 
     # Iterate through each page, convert it to an image, and call Textract
     for page_number in range(len(pdf_document)):
+        t0 = time.time()
         page = pdf_document.load_page(page_number)
         pix = page.get_pixmap()
         img_byte_arr = io.BytesIO(pix.tobytes(output="jpeg"))  # Convert pixmap to JPEG bytes
         img = io.BytesIO(img_byte_arr.getvalue())  # Prepare for sending to Textract
-
+        t1 = time.time() 
+        logger.info(f"Time taken for image conversion: {t1-t0:.6f} seconds")
         # Call Textract for each page image
         textract_result = textract_client.detect_document_text(Document={"Bytes": img.getvalue()})
+        t2 = time.time()
+        logger.info(f"Time taken for Textract: {t2-t1:.6f} seconds")
         textract_pages.append(response_parser.parse(textract_result))
     
     pdf_document.close()
@@ -36,31 +49,26 @@ def get_document_text(pdf_content):
 
 def handler(event, context):
     # Extract bucket name and object key from the S3 event
-    
-    start_time = time.time()
-    
+        
     logger.info(f"Event: {event}")
     
     bucket_name = event.get("detail").get("bucket").get("name")
     object_key = event.get("detail").get("object").get("key")
 
     # Get the PDF object from S3
+    t0 = time.time()
     response = s3_client.get_object(Bucket=bucket_name, Key=object_key)
     pdf_content = response['Body'].read()
+    t1 = time.time()
+    logger.info(f"Time taken for S3 GetObject: {t1-t0:.6f} seconds")
 
     response = get_document_text(pdf_content)
-    
+
     document_str = ""
     for doc in response:
         document_str = document_str + doc.text
         
-    logger.info(f"Document_text: {document_str}")
-    
-    end_time = time.time()
-    elapsed_time = end_time - start_time
-    
-    logger.info(f"Time taken for the detect text lambda: {elapsed_time:.6f} seconds")
-    
-    response = {"document_text": document_str, "bucket_name": bucket_name, "object_key": object_key}
+    logger.info(f"Document_text: {document_str}")   
+    response = {"input_bucket_name": bucket_name, "input_object_key": object_key, "document_text": document_str}
 
     return response 
