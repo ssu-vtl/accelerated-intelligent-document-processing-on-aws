ee49461bbeae624ab443bbf051be2c20140d2807
Bob Strahan <strahans@amazon.com>
2024-12-17 21:26:15 +0000

    add shebang

--- scripts/simulate_dynamic_load.py
+++ scripts/simulate_dynamic_load.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # Copyright © Amazon.com and Affiliates: This deliverable is considered Developed Content as defined in the AWS Service Terms and the SOW between the parties.
 
 import boto3
