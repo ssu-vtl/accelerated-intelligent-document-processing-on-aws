1df690dec76cefd4a1f4f4577d50b65c04e42a56
Bob Strahan <strahans@amazon.com>
2025-02-04 18:06:45 +0000

    fix to support any level of nesting in input URL

--- src/ui/src/components/common/generate-s3-presigned-url.js
+++ src/ui/src/components/common/generate-s3-presigned-url.js
@@ -6,33 +6,46 @@ import { parseUrl } from '@aws-sdk/url-parser';
 import { Sha256 } from '@aws-crypto/sha256-browser';
 import { formatUrl } from '@aws-sdk/util-format-url';
 
-let newUrl = '';
-
 const generateS3PresignedUrl = async (url, credentials) => {
-  // prettier-ignore
-
-  const bucketName = url.split('/')[2].split('.')[0];
-  const key = `${url.split('/')[3]}/${url.split('/')[4]}`;
-  const region = url.split('/')[2].split('.')[2];
-
-  newUrl = `https://${bucketName}.s3.${region}.amazonaws.com/${key}`;
-
+  // If it's already a special URL (like detailType), return as is
   if (url.includes('detailType')) {
-    newUrl = url;
+    return url;
   }
 
-  // const s3ObjectUrl = parseUrl(`https://${bucketName}.s3.${region}.amazonaws.com/${key}`);
-  const s3ObjectUrl = parseUrl(newUrl);
+  try {
+    // Parse the URL into components
+    const urlObj = new URL(url);
 
-  const presigner = new S3RequestPresigner({
-    credentials,
-    region,
-    sha256: Sha256, // In browsers
-  });
-  // Create a GET request from S3 url.
-  const presignedResponse = await presigner.presign(new HttpRequest(s3ObjectUrl));
-  const presignedUrl = formatUrl(presignedResponse);
-  return presignedUrl;
+    // Extract bucket name from hostname
+    const bucketName = urlObj.hostname.split('.')[0];
+
+    // Extract region from hostname
+    const region = urlObj.hostname.split('.')[2];
+
+    // Remove leading slash and get the full key path
+    const key = urlObj.pathname.substring(1);
+
+    // Construct the canonical S3 URL
+    const newUrl = `https://${bucketName}.s3.${region}.amazonaws.com/${key}`;
+
+    // Parse the URL for the presigner
+    const s3ObjectUrl = parseUrl(newUrl);
+
+    // Create presigner instance
+    const presigner = new S3RequestPresigner({
+      credentials,
+      region,
+      sha256: Sha256,
+    });
+
+    // Generate presigned URL
+    const presignedResponse = await presigner.presign(new HttpRequest(s3ObjectUrl));
+    const presignedUrl = formatUrl(presignedResponse);
+
+    return presignedUrl;
+  } catch (error) {
+    throw new Error(`Failed to generate presigned URL: ${error.message}`);
+  }
 };
 
 export default generateS3PresignedUrl;
