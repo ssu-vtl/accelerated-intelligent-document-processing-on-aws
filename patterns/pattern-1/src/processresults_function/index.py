dfbcb4a61ca9b9679164d3d55960b67f85798940
Bob Strahan <strahans@amazon.com>
2025-04-07 22:19:50 +0000

    fix missing os import

--- patterns/pattern-1/src/processresults_function/index.py
+++ patterns/pattern-1/src/processresults_function/index.py
@@ -1,6 +1,6 @@
 # Copyright © Amazon.com and Affiliates: This deliverable is considered Developed Content as defined in the AWS Service Terms and the SOW between the parties.
 import json
-import boto3
+import os
 from botocore.exceptions import ClientError
 from typing import Dict, Any, List
 import logging
