28da93dcdfa45d4bbad2ccb7142c260457e09219
Bob Strahan <strahans@amazon.com>
2025-04-10 19:05:06 +0000

    fix import bug

--- lib/idp_common_pkg/idp_common/ocr/__init__.py
+++ lib/idp_common_pkg/idp_common/ocr/__init__.py
@@ -5,6 +5,5 @@ Provides a service for processing PDF documents with AWS Textract.
 """
 
 from idp_common.ocr.service import OcrService
-from idp_common.ocr.results import OcrPageResult, OcrDocumentResult
 
-__all__ = ['OcrService', 'OcrPageResult', 'OcrDocumentResult']
+__all__ = ['OcrService']
