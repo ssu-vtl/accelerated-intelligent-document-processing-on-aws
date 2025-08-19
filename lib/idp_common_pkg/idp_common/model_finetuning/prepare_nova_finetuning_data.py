9042d04e57c053038b69ec51d499bfe0c7f39491
Bob Strahan <strahans@amazon.com>
2025-08-19 19:49:29 +0000

    Update copyright headers to MIT-0 license in model_finetuning and reporting test modules

--- lib/idp_common_pkg/idp_common/model_finetuning/prepare_nova_finetuning_data.py
+++ lib/idp_common_pkg/idp_common/model_finetuning/prepare_nova_finetuning_data.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
-#
-# Copyright Amazon.com, Inc. or its affiliates. This material is AWS Content under the AWS Enterprise Agreement
-# or AWS Customer Agreement (as applicable) and is provided under the AWS Intellectual Property License.
-#
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: MIT-0
 """
 Nova Lite Fine-tuning Dataset Preparation Script
 
