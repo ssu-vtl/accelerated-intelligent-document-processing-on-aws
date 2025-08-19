08fbe14d4fdb0e8d09ceea771ad9acde1a88e025
Bob Strahan <strahans@amazon.com>
2025-08-19 17:19:32 +0000

    Add copyright headers and configure log retention for CloudWatch logs

--- lib/idp_common_pkg/idp_common/model_finetuning/create_provisioned_throughput.py
+++ lib/idp_common_pkg/idp_common/model_finetuning/create_provisioned_throughput.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+#
+# Copyright Amazon.com, Inc. or its affiliates. This material is AWS Content under the AWS Enterprise Agreement
+# or AWS Customer Agreement (as applicable) and is provided under the AWS Intellectual Property License.
+#
 """
 Nova Provisioned Throughput Creation Script
 
