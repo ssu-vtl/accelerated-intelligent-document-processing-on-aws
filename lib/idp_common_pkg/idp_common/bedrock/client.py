57793efb274725a5b0f5ff27531d86377af8a9dc
EC2 Default User <ec2-user@ip-172-31-23-7.us-east-2.compute.internal>
2025-09-03 14:59:15 +0000

    changed the metering data to take modelId

--- lib/idp_common_pkg/idp_common/bedrock/client.py
+++ lib/idp_common_pkg/idp_common/bedrock/client.py
@@ -458,7 +458,7 @@ class BedrockClient:
             response_with_metering = {
                 "response": response,
                 "metering": {
-                    f"{context}/bedrock/{model_id}": {
+                    f"{context}/bedrock/{'modelId'}": {
                         **usage
                     }
                 }
